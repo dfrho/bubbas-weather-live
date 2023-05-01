a42e74fa8d9bcc954977f52fac61c07acbf41741
David Rhodes <david@espressocode.tech>
2023-04-30 20:46:15 -0500

    initial commit

---
+++ pages/api/getAdvice.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  const { weatherData } = req.body;
+
+  if (weatherData) {
+    try {
+      const openaiResponse = await axios.post(
+        'https://openai-weather-7bezhl2tn-dfrho.vercel.app/api/advice',
+        {
+          prompt: `In the voice of Willard Scott without saying so, and based on last update at ${weatherData.location.localtime} and weather conditions consisting of Cloud Coverage is ${weatherData.current.cloud}% and Temperature is ${weatherData.current.temp_f}F and Humidity is ${weatherData.current.humidity}% and Precipitation today is ${weatherData.current.precip_in} inches and Current Wind Speed is ${weatherData.current.wind_mph} mph and Winds are Gusting at ${weatherData.current.gust_mph} mph, any advice on what a web user looking at your response in a pop up window, should pack for the day, as in outerwear, sunglasses, and/or an umbrella?`,
+          stop: ['\\n'],
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      const advice = openaiResponse.data.choices[0].text.trim();
+      res.status(200).json({ advice });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ message: 'An error occurred while fetching advice' });
+    }
+  } else {
+    res.status(400).json({ message: 'Invalid request data' });
+  }
+}
---
+++ pages/api/getWeatherData.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  const { ipAddress } = req.query;
+
+  try {
+    const response = await axios.get(
+      `https://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_APIKEY}&q=${ipAddress}`
+    );
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: 'An error occurred while fetching weather data' });
+  }
+}

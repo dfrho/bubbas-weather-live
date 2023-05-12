b609eced868bc209058e381e46df290eaab630de
David Rhodes <david@espressocode.tech>
2023-05-11 22:01:35 -0500

    limit prompt

--- pages/api/getAdvice.js
+++ pages/api/getAdvice.js
@@ -18,7 +18,7 @@ async function handler(req, res) {
   const { weatherData } = req.body;
 
   if (weatherData) {
-    const prompt = `In the voice of Willard Scott without saying so, and presence based on current time as ${weatherData.location.localtime} and current weather described as ${weatherData.current.condition.text} and weather conditions consisting of Cloud Coverage is ${weatherData.current.cloud}% and Temperature is ${weatherData.current.temp_f}F and Humidity is ${weatherData.current.humidity}% and Precipitation for the day is ${weatherData.current.precip_in} inches and Current Wind Speed is ${weatherData.current.wind_mph} mph and Winds are Gusting at ${weatherData.current.gust_mph} mph, any advice on what a web user should pack when going outside, as in outerwear, and/or an umbrella?`;
+    const prompt = `In the voice of Willard Scott without saying so, and presence based on current time as ${weatherData.location.localtime} and based on current weather described as ${weatherData.current.condition.text} and weather conditions consisting of Cloud Coverage is ${weatherData.current.cloud}% and Temperature is ${weatherData.current.temp_f}F and Humidity is ${weatherData.current.humidity}% and Precipitation for the day is ${weatherData.current.precip_in} inches and Current Wind Speed is ${weatherData.current.wind_mph} mph and Winds are Gusting at ${weatherData.current.gust_mph} mph, any advice on what I should pack when going outside, as in outerwear, and/or an umbrella? Limit 140 characters.`;
     try {
       const { data } = await openai.createCompletion({
         model: 'text-davinci-003',
@@ -30,7 +30,7 @@ async function handler(req, res) {
         presence_penalty: 0.6, // Fine-tune to encourage relevant responses
         stop: '\\n',
       });
-      const advice = addPeriod(data.choices[0].text);
+      const advice = data.choices[0].text.trim();
       res.status(200).json({ advice });
     } catch (error) {
       console.error(error);

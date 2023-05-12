a125513682bdf1ef6e9c3472f670cfe128157dde
David Rhodes <david@espressocode.tech>
2023-05-11 19:10:59 -0500

    testing

--- pages/api/getWeatherData.js
+++ pages/api/getWeatherData.js
@@ -2,10 +2,14 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   const { ipAddress } = req.query;
+  console.log(
+    '🚀 ~ file: getWeatherData.js:5 ~ handler ~ ipAddress:',
+    ipAddress
+  );
 
   try {
     const response = await axios.get(
-      `https://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_APIKEY}&q=${ipAddress}`
+      `https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHERAPI_APIKEY}&q=${ipAddress}`
     );
 
     res.status(200).json(response.data);
